06aac8d707cd67e1fea1cadc4d63c7c0706c259a
ISHIMWE Deborah <ishimwedebby53@gmail.com>
2025-04-06 20:20:34 +0200

    collection codes in java

--- JAVA_PROGRAMS/ARRAYS/src/collection/CollectionApp.java
+++ JAVA_PROGRAMS/ARRAYS/src/collection/CollectionApp.java
@@ -10,13 +10,13 @@ public class CollectionApp {
 //        c1.add("70");
 //        System.out.println(c1);
 
-        Set<Integer> c1=new HashSet<>();
-        c1.add(50);
-        c1.add(20);
-        c1.add(40);
-        c1.add(9);
-
-        Set<Integer> c2=new HashSet<>(Arrays.asList(50,9,11,7));
+//        Set<Integer> c1=new HashSet<>();
+//        c1.add(50);
+//        c1.add(20);
+//        c1.add(40);
+//        c1.add(9);
+//
+//        Set<Integer> c2=new HashSet<>(Arrays.asList(50,9,11,7));
 //        union
 //       Set<Integer> c3=new HashSet<>(c1);
 //        System.out.println(c3);
@@ -29,10 +29,32 @@ public class CollectionApp {
 //        c4.retainAll(c2);
 //        System.out.println(c4);
 
-        Set<Integer>s5=new HashSet<>(c1);
-        System.out.println(s5);
-        s5.removeAll(c2);
-        System.out.println(s5);
+//        Set<Integer>s5=new HashSet<>(c1);
+//        System.out.println(s5);
+//        s5.removeAll(c2);
+//        System.out.println(s5);
+        double product=1.1;
+
+        Set<Double> s1=new TreeSet<>();
+        double sum=0.0;
+        s1.add(10.0);
+        s1.add(40.0);
+//        System.out.println(s1);
+
+//        iterating though set elements
+//        1.for each loop
+        for(double m :s1){
+            sum+=m;
+            System.out.println("the sum is "+sum);
+        }
+//        2.using iterator()method
+        Iterator<Double>itr=s1.iterator();
+//        System.out.println(itr.next());
+        while (itr.hasNext()){
+//            System.out.println(itr.next());
+            product *=itr.next();
+        }
+        System.out.println("product"+product);
 
 
 
